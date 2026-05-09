13aaa8139ce12ccd8d9a265b65a1f37199ca4a91
youssefazazy6969-sketch <youssefazazy6969@gmail.com>
2026-05-09 17:33:26 +0300

    Create day_9.cpp

---
+++ day_9.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    if (1==n)   cout << "one" << endl;
+    if (2==n)   cout << "two" << endl;
+    if (3==n)   cout << "three" << endl;
+    if (4==n)   cout << "four" << endl;
+    if (5==n)   cout << "five" << endl;     
+    if (6==n)   cout << "six" << endl;
+    if (7==n)   cout << "seven" << endl;
+    if (8==n)   cout << "eight" << endl;
+    if (9==n)   cout << "nine" << endl;
+    if (10==n)  cout << "Greater than 9" << endl;
+    if (44==n)   cout << "Greater than 9" << endl;
+    if (100==n)   cout << "Greater than 9" << endl;
+    
+    return 0;
+}
+/*today finished level 5 of The English course AUC, finished 4 lessons of Elzero course and
+ submitted 1 solution on hackerrank*/
